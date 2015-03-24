e22dca75038cee6fa892b18a7941deec50cadbb4
ahmed-emam <aemam.douh@mail.com>
2015-03-24 14:43:00 +0300

    first blood

---
+++ agl/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    faction_choices = (
+        ('WH', "White"),
+        ('DK', "Dark"),
+    )
+    faction = models.CharField(max_length=2, choices=faction_choices, default='WH')
